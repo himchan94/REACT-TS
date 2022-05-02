3f144e72a1c718c6f7a75736623f7a20c3a8e302
himchan94 <himchan9494@gmail.com>
2022-05-02 23:43:02 +0900

    feat: Enum & Types

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,15 +1,27 @@
 import { FC, ChangeEvent, useState } from "react";
 
+// enum defines options
+export enum HairColor {
+  Blonde = "Your hair is blond, good for you",
+  Brown = "Coll hair color",
+  Pink = "Wow that is so cool",
+}
+
 // interface is for object type
 export interface Props {
   name?: string;
   age?: number;
   email?: string;
+  hairColor: HairColor;
   getname?: (name: string) => string;
 }
 
-export const Person: FC<Props> = ({ name, email, age }) => {
+export const Person: FC<Props> = ({ name, email, age, hairColor }) => {
   const [country, setCountry] = useState<string | null>(null);
+
+  type NameType = "Himchan" | "Jason";
+
+  const nameForType: NameType = "Himchan";
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setCountry(event.target.value);
@@ -21,7 +33,10 @@ export const Person: FC<Props> = ({ name, email, age }) => {
       <h1>{age}</h1>
       <h1>{email}</h1>
       <input placeholder='write down your country..' onChange={handleChange} />
+      <br />
       {country}
+      <br />
+      {hairColor}
     </div>
   );
 };
