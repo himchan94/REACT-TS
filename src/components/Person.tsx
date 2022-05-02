119aeb3ae1ef7d84627ba65bb33fa9fa73de887e
himchan94 <himchan9494@gmail.com>
2022-05-02 23:34:08 +0900

    feat: Typing event

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, ChangeEvent, useState } from "react";
 
 // interface is for object type
 export interface Props {
@@ -9,11 +9,19 @@ export interface Props {
 }
 
 export const Person: FC<Props> = ({ name, email, age }) => {
+  const [country, setCountry] = useState<string | null>(null);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setCountry(event.target.value);
+  };
+
   return (
     <div>
       <h1>{name}</h1>
       <h1>{age}</h1>
       <h1>{email}</h1>
+      <input placeholder='write down your country..' onChange={handleChange} />
+      {country}
     </div>
   );
 };
